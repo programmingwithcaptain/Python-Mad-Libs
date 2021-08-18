f37f642e392d5a32d50d31f7f7c707ff89ddd0fc
programmingwithcaptain <89128567+programmingwithcaptain@users.noreply.github.com>
2021-08-18 13:14:46 +0500

    Mad Libs in Python

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+# creating a little mad libs game
+
+color = input("Enter a color: ")
+noun = input("Enter a noun: ")
+noun1 = input("Enter another noun: ")
+
+print("Roses are " + color)
+print(noun + " are Blue")
+print("I love " + noun1)
